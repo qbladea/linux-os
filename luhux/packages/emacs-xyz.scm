9dbc5c8d973f03a3d839b582f7f54cf4e32bf6d8
qblade <qblade@protonmail.com>
2021-03-26 01:29:50 +0000

    packages: 规范打包 emacs-sdcv emacs-showtip

--- luhux/packages/emacs-xyz.scm
+++ luhux/packages/emacs-xyz.scm
@@ -15,20 +15,20 @@
      (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/emacsorphanage/showtip.git")
+             (url "https://github.com/emacsorphanage/showtip")
              (commit
               "930da302809a4257e8d69425455b29e1cc91949b")))
+       (file-name (git-file-name name version))
        (sha256
         (base32
          "01zak0zhha6dp7a2hm28d065gjnc462iwpsfyxhbxgfzcdlicqc7"))))
     (build-system emacs-build-system)
-    (home-page "unspecified")
+    (home-page "https://github.com/emacsorphanage/showtip")
     (synopsis "Show tip at cursor")
     (description
      "This library export one function for elisp programer to show
-tooltip near the cursor not the mouse.
-")
-    (license #f)))
+tooltip near the cursor not the mouse.")
+    (license license:gpl2+)))
 
 (define-public emacs-sdcv
   (package
@@ -38,9 +38,10 @@ tooltip near the cursor not the mouse.
      (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://github.com/stardiviner/sdcv.el.git")
+             (url "https://github.com/stardiviner/sdcv.el")
              (commit
               "943ae3e90cc9a0a88a37cc710acd7424fd4defc4")))
+       (file-name (git-file-name name version))
        (sha256
         (base32
          "0i1ylvw7p46pkf3yyyzcdmdhsspzymnnnvx8s0i7vynngr5x0vzh"))))
@@ -50,10 +51,10 @@ tooltip near the cursor not the mouse.
        ("emacs-showtip" ,emacs-showtip)
        ("emacs-pos-tip" ,emacs-pos-tip)))
     (home-page
-     "http://www.emacswiki.org/emacs/download/sdcv.el")
+     "https://www.emacswiki.org/emacs/download/sdcv.el")
     (synopsis
-     "Interface for sdcv (StartDict console version).")
+     "Emacs interface for sdcv")
     (description
      "Translate word by sdcv (console version of Stardict), and display
 translation use popup tooltip or buffer.")
-    (license #f)))
+    (license license:gpl3+)))
